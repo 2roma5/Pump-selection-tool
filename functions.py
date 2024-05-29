457fdd6ed7440e368ccbef2232717aac5c458b6d
konor <emilioxkonor@gmail.com>
2024-05-28 18:03:24 -0600

    1st pull request

--- functions.py
+++ functions.py
@@ -1,23 +1,26 @@
 import matplotlib.pyplot as plt
+from math import log10
+
 
 def Hb(pressure_1, pressure_2, density_1):
     (pressure_2-pressure_1)/(density_1*9.81)
 
 
 def Hs(pressure_1, pressure_2, density_1, velocity_1, velocity_2,
-       z_1, z_2, frictions: list):
+       z_1, z_2, frictions):
     dp = pressure_2-pressure_1
     dz = z_2-z_1
     dv = velocity_2**2-velocity_1**2
-    return (dp)/(density_1*9.81) + (dz) + (dv)/(2*9.81) + sum(frictions)
+    return (dp)/(density_1*9.81) + (dz) + (dv)/(2*9.81) + frictions
 
 
 def Re(velocity, diameter, density, viscosity):
     return velocity*diameter*density/viscosity
 
 
-def factor_friction():
-    return 1
+def factor_friction(diameter, roughness, Reynolds):
+    den = log10(1/(3.7*diameter/roughness) + 5.74/(Reynolds**0.9))
+    return 0.25/(den**2)
 
 
 def plot_data(x: list, y: list, title: str, xlabel: str, ylabel: str) -> None:
