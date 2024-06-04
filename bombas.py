cb43f7a9bef368394559fa666c773ed037046635
2Roma5 <319323850@quimica.unam.mx>
2024-06-03 22:33:52 -0600

    Organización de código y descripciones

--- bombas.py
+++ bombas.py
@@ -4,7 +4,7 @@ import pandas as pd
 from functions import Re, factor_friction, velocity, Hs
 from functions import plot_data, friction, len_eq
 
-# Variables del proceso
+# Accesorios
 accesorios = {
     "vc": 0,
     "vg": 0,
@@ -17,26 +17,26 @@ accesorios = {
     "cc": 0,
 }
 
+# Leq = (K*D)/f
 len = {
-    "vc": 8*(2.0670/39.37),
-    "vg": 340*(2.0670/39.37),
-    "vb": 150*(2.0670/39.37),
-    "vm": 45*(2.0670/39.37),
-    "vn": 100*(2.0670/39.37),
-    "po": 10*(2.0670/39.37),
-    "r": 10*(2.0670/39.37),
-    "tp": 10*(2.0670/39.37),
-    "cc": 30*(2.0670/39.37)
+    "vc": (0.15*0.052)/0.019, 
+    "vg": (6.5*0.052)/0.019, 
+    "vb": (0.1*0.052)/0.019, 
+    "vm": (0.7*0.052)/0.019, 
+    "vn": (2.75*0.052)/0.019,
+    "po": (0.1*0.052)/0.019,
+    "r": (7*0.052)/0.019,
+    "tp": (7*0.052)/0.019,
+    "cc": (0.7*0.052)/0.019
 }
 
 # Catálogo de accesorios (solo imprime las opciones)
 def catalogo():
-    print("Seleccione los accesorios presentes en su proceso: ")
+    print("\nSeleccione los accesorios presentes en su proceso: ")
     print("------------------------------------------------\n")
     print("\t     CATÁLOGO DE ACCESORIOS\n")
     print("1. Válvula de compuerta.  6. Placa de orificio.\n2. Válvula de globo.      7. Rotámetro.\n3. Válvula de bola.       8. Tubo pitot.\n4. Válvula de mariposa.   9. Codos\n5. Válvula check.         10. Salir.") 
     print("\n------------------------------------------------")
-
 
 # Consigue las variables del proceso (accesorios)
 def get_process(accesorios: dict[str, int]):
@@ -44,7 +44,7 @@ def get_process(accesorios: dict[str, int]):
         try:
             op = int(input("opción: "))
             if op == 1:
-                c = int(input("¿Cuántas válvulas de compuesta tiene?: "))
+                c = int(input("¿Cuántas válvulas de compuerta tiene?: "))
                 accesorios["vc"] = accesorios["vc"] + c
                 get_process(accesorios)
             elif op == 2:
@@ -90,44 +90,43 @@ def get_process(accesorios: dict[str, int]):
             print("Solo se admiten números, inténtelo de nuevo.")
 
 # Consigue los requerimientos del proceso
-def get_info(accesorios: dict[str, int]):
+def get_info():
     print("¡Bienvenido, aquí podrás elegir la mejor bomba para tu proceso!")
     time.sleep(1.5)
     print("Por favor ingresa los requerimientos y características de tu proceso.")
-    q = float(input("Ingresa el flujo que necesita tu proceso [L/min]: "))
-    if q < 0:
-        q = abs(q)
+    caudal = float(input("Ingresa el flujo que necesita tu proceso [L/min]: "))
+    if caudal < 0:
+        caudal = abs(caudal)
         print("¿Negativo?")
-    catalogo()
-    get_process(accesorios)
-    idk()
+    global z1, z2, l, diametro
+    z1 = float(input("Ingresa la elevación inicial [m]: "))
+    z2 = float(input("Ingresa la elevación final [m]: "))
+    l = float(input("Ingrese la longuitud de tubería recta [m]: "))
+    diametro = float(input("Ingrese el diámetro de su tubería [in]: "))
+    "\n"
+    diametro = diametro/39.37
 
-
+# Calcula la cabeza del sistema y la presenta 
 def idk():
-    z1 = float(input("Ingresa la elevación inicial: "))
-    z2 = float(input("Ingresa la elevación final: "))
-    l = float(input("Ingrese la longuitud de tubería recta: "))
-    #z1 = float(z1)
-    #z2 = float(z2)
-    z1 = 120.5/100
-    z2 = 316/100
-    Q = np.linspace(0, 1000, 1000)
-    Q_ = Q/1000/60
+    Q = np.linspace(0, 200, 1000)
+    Q_ = Q/200/60
     densidad = 998.2
     viscosidad = 0.00105
-    diametro = 2.0670/39.37
     longuitudT = len_eq(accesorios, len) + l
     velocidades = [velocity(q, diametro) for q in Q_]
     Reynolds = [Re(v, diametro, densidad, viscosidad) for v in velocidades]
     f_fricc = [factor_friction(diametro, 0.000046, r) for r in Reynolds]
     fricciones = [friction(f, diametro, v, longuitudT) for f, v in zip(
         f_fricc, velocidades)]
-    cabeza = [Hs(0, 1, 998.2, v, v, z1, z2, f) for v, f in zip(
+    cabeza = [Hs(1, 1, 998.2, v, v, z1, z2, f) for v, f in zip(
         velocidades, fricciones)]
     plot_data(Q, cabeza, "Cabeza vs Flujo", "Flujo [L/min]", "Cabeza [m]")
     df = pd.DataFrame({"Velocidad": velocidades, "Reynolds": Reynolds,
                        "Factor de fricciones": f_fricc, "Cabeza": cabeza,
                        "Fricciones": fricciones})
-    print(df)
     input("Presiona Enter para continuar...")
 
+get_info()
+catalogo()
+get_process(accesorios)
+idk()
--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 from math import log10
 
-
+# Calcula la cabeza de la bomba
 def Hb(pressure_1, pressure_2, density_1):
     (pressure_2-pressure_1)/(density_1*9.81)
 
-
+# Calcula la cabeza del sistema
 def Hs(pressure_1, pressure_2, density_1, velocity_1, velocity_2,
        z_1, z_2, frictions):
     dp = pressure_2-pressure_1
@@ -13,16 +13,16 @@ def Hs(pressure_1, pressure_2, density_1, velocity_1, velocity_2,
     dv = velocity_2**2-velocity_1**2
     return (dp*100**2)/(density_1) + (dz) + (dv)/(2*9.81) + frictions
 
-
+# Calcula el número de Reynolds
 def Re(velocity, diameter, density, viscosity):
     return velocity*diameter*density/viscosity
 
-
+# Calcula los factores de fricción
 def factor_friction(diameter, roughness, Reynolds):
     den = 0.25/(log10(1/(3.7*diameter/roughness)) + 5.74/Reynolds**0.9)**2
     return den
 
-
+# Calcula las pérdidas de cabeza
 def friction(f_friction, diameter, velocity, length):
     return f_friction*length*velocity**2/(diameter*2*9.81)
 
